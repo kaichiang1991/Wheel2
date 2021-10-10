49d298d1ef685a3b39d27fd698df55bf146c1a6a
Kai <addie60111@gmail.com>
2021-10-10 11:06:27 +0800

    切好遊戲畫面

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
-import { StyledGameWrapper } from './index.style'
+import { 
+    StyledGameWrapper, 
+    StyledListContainer,
+    StyledGameButton
+} from './index.style'
 import {
+    dataArrState,
     titleState
 } from '../../Recoil'
+import { useHistory } from 'react-router'
+import { nanoid } from 'nanoid'
+
+const ListItem = props => {
+    const {name, count} = props
+    return (
+        <li>
+            <span className="name">{name}</span>
+            <span className="count">{count} 個</span>
+        </li>
+    )
+}
 
 const Game = () => {
     const title = useRecoilValue(titleState)
+    const dataArr = useRecoilValue(dataArrState)
+
+    const history = useHistory()
+    useEffect(()=>{
+        // 避免重新整理後沒資料，回到上一頁
+        if(!title || !dataArr.length){
+            // history.goBack()
+        }
+    }, [])
+
     return (
         <StyledGameWrapper>
             <div className="middle">
                 <h1>{title}</h1>
             </div>
-            <div className="left">111</div>
-            <div className="right">222</div>
+            <div className="game-div">
+                <div className="canvas-container">111</div>
+                <StyledListContainer>
+                    {
+                        dataArr.map(data => <ListItem key={nanoid()} {...data} />)
+                    }
+                </StyledListContainer>
+            </div>
+            <div className="ui-container">
+                <StyledGameButton className="start" children={'抽獎'}/>
+                <StyledGameButton className="export" children="導出至excel"/>
+            </div>
         </StyledGameWrapper>
     )
 }
--- src/pages/Game/index.style.js
+++ src/pages/Game/index.style.js
@@ -1,19 +1,68 @@
 import styled from "styled-components";
 
 export const StyledGameWrapper = styled.div`
+    height: 100%;
+    width: 100%;
+
     .middle{
         text-align: center;
     }
 
-    .left{
+    .canvas-container{
         float: left;
         width: 40%;
-        background-color: red;
+        height: 100%;
+        border: solid 1px black;
     }
 
-    .right{
-        float: left;
-        width: 60%;
-        background-color: green;
+    .game-div{
+        height: 70%
     }
+
+    .game-div:after{
+        content: '';
+        clear: both;
+        display: block;
+    }
+
+    .ui-container{
+        top: 10px;
+    }
+
+    .ui-container .start{
+        left: 50%;
+        transform: translateX(-50%)
+    }
+
+    .ui-container .export{
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        margin-bottom: 4px;
+    }
+`
+
+export const StyledListContainer = styled.ul`
+    float: right;
+    width: 30%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, .4);
+    overflow: auto;
+
+    li{
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    li span{
+        padding: 8px;
+    }
+`
+
+export const StyledGameButton = styled.button`
+    height: 48px;
+    margin: 0 8px;
+    padding: 4px 20px;
+    box-shadow: black 4px 4px 4px;
 `
